52fcf55b4037f40dbd63f403d014edfb26b18e3e
Atanas Andreev <apleced@gmail.com>
2020-04-16 16:35:38 +0300

    Initial project

---
+++ jobprocessor/src/test/java/com/sumup/jobprocessor/JobprocessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumup.jobprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobprocessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
